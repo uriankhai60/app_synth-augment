ddd140fe748be5fd61568c5b7458625adc294537
uriankhai60 <uriankhai60@gmail.com>
2025-11-11 05:11:53 +0000

    [Feat] update qwen-image

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain.output_parsers import StructuredOutputParser
 from utils import *
 from models import *
 from misc import *
-
+from tqdm import tqdm
 import os
 import random
 
@@ -50,7 +50,7 @@ def generate_candidate_prompts(model: str, prompt: str, num_captions: int) -> Li
     ]
 
     # gpt model ready
-    chat_llm = ChatOpenAI(model=model, temperature=0.2)
+    chat_llm = ChatOpenAI(model=model, temperature=0.4)
 
     # api call
     gpt_response = chat_llm.invoke(messages)
@@ -60,80 +60,61 @@ def generate_candidate_prompts(model: str, prompt: str, num_captions: int) -> Li
     return outs["captions"]
 
 
-def generation_image(
-    model_name: str, caption: str, width: int, height: int, ratio: str
-) -> Image.Image:
+def generation_image(model_name: str, caption: str, ratio: str) -> Image.Image:
     out: Image.Image = None
     if model_name == "fal-ai/nano-banana":
         out = get_image_from_nano_banana(caption, ratio)
-    elif model_name == "...":
-        ...
-    elif model_name == "...":
-        ...
+    elif model_name == "qwen-image":
+        out = get_image_from_qwen(caption, ratio)
     else:
         raise ValueError(f"model_name: {model_name} is not implement")
     return out
 
 
-def main():
-    input_prompt = "사실적인 2024년의 한국인을 만들어달라, 이미지는 얼굴이 잘 나와있어야 하며, 이미지 속에는 단 한명의 인물만 있어야 한다"
-    n_image = 5
-    caption_gen_model = "gpt-4"
-    image_gen_model = "fal-ai/nano-banana"
-    output_dir = "generated_images"
+def main(n_image=5):
+    input_prompt = "사실적인 현대 아시아인을 만들어달라, 이미지는 얼굴이 잘 나와있어야 하며, 이미지 속에는 단 한명의 인물만 있어야 한다"
+    n_image = n_image
+    caption_gen_model = "gpt-4o-mini"
+    image_gen_model = "qwen-image"
+    output_dir = "generated_images__qwen-image_n6000"
 
     # generate image gen prompts(=captions)
     captions = generate_candidate_prompts(caption_gen_model, input_prompt, n_image)
 
-    meta = {}
     gen_images = []
     for idx, caption in enumerate(captions):
-
-        # select ratio and resolution
-        selected = random.choice(
+        # select ratio
+        ratio = random.choice(
             [
-                {"1:1": (1024, 1024)},
-                {"4:5": (912, 1140)},
-                {"3:4": (885, 1180)},
-                {"3:2": (1254, 836)},
-                {"16:9": (1360, 765)},
+                "1:1",
+                "16:9",
+                "9:16",
+                "4:3",
+                "3:4",
+                "3:2",
+                "2:3",
             ]
         )
-        ratio, (width, height) = next(iter(selected.items()))
 
         # image_generation
         gen_image = generation_image(
             model_name=image_gen_model,
             caption=caption,
-            width=width,
-            height=height,
             ratio=ratio,
         )
         # append image
         gen_images.append(gen_image)
-        # save meta info
-        meta[idx] = {
-            "filename": current_timestamp() + ".png",
-            "caption": caption,
-            "resolution": gen_image.size,
-            "ratio": ratio,
-            "model_name": image_gen_model,
-        }
 
     # prepare output dir
     output_dir = Path(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    # save meta
-    with open(output_dir / "meta.json", "w", encoding="utf-8") as f:
-        json.dump(meta, f, ensure_ascii=False, indent=2)
-
     # save image
-    for idx in range(len(meta)):
-        pil_image = gen_images[idx]
-        filename = meta.get(idx).get("filename")
-        pil_image.save(output_dir / filename)
+    for idx, gen_image in enumerate(gen_images):
+        filename = current_timestamp() + f"_{idx}" + ".jpg"
+        gen_image.save(output_dir / filename)
 
 
 if __name__ == "__main__":
-    main()
+    for _ in tqdm(range(500)):
+        main(n_image=12)
--- models/image_gen_models.py
+++ models/image_gen_models.py
@@ -1,10 +1,41 @@
 import os
 from io import BytesIO
-
+from diffusers import DiffusionPipeline
 import fal_client
 import requests
 from PIL import Image
+import torch
+from diffusers import DiffusionPipeline, FlowMatchEulerDiscreteScheduler
+import math
 
+model_name = "Qwen/Qwen-Image"
+torch_dtype = torch.bfloat16
+device = "cuda"
+
+## prepare
+scheduler_config = {
+    "base_image_seq_len": 256,
+    "base_shift": math.log(3),  # We use shift=3 in distillation
+    "invert_sigmas": False,
+    "max_image_seq_len": 8192,
+    "max_shift": math.log(3),  # We use shift=3 in distillation
+    "num_train_timesteps": 1000,
+    "shift": 1.0,
+    "shift_terminal": None,  # set shift_terminal to None
+    "stochastic_sampling": False,
+    "time_shift_type": "exponential",
+    "use_beta_sigmas": False,
+    "use_dynamic_shifting": True,
+    "use_exponential_sigmas": False,
+    "use_karras_sigmas": False,
+}
+scheduler = FlowMatchEulerDiscreteScheduler.from_config(scheduler_config)
+pipe = DiffusionPipeline.from_pretrained(
+    "Qwen/Qwen-Image", scheduler=scheduler, torch_dtype=torch.bfloat16
+).to("cuda")
+pipe.load_lora_weights(
+    "lightx2v/Qwen-Image-Lightning", weight_name="Qwen-Image-Lightning-8steps-V1.0.safetensors"
+)
 
 def get_image_from_nano_banana(prompt: str, aspect_ratio: str) -> Image.Image:
     """fal-ai nano-banana 모델을 동기적으로 호출해 이미지를 반환한다."""
@@ -18,3 +49,25 @@ def get_image_from_nano_banana(prompt: str, aspect_ratio: str) -> Image.Image:
     image_url = result["images"][0]["url"]
     response = requests.get(image_url)
     return Image.open(BytesIO(response.content)).convert("RGB")
+
+
+def get_image_from_qwen(prompt:str, ratio:str) -> Image.Image:
+    aspect_ratios = {
+    "1:1": (1328, 1328),
+    "16:9": (1664, 928),
+    "9:16": (928, 1664),
+    "4:3": (1472, 1140),
+    "3:4": (1140, 1472),
+    "3:2": (1584, 1056),
+    "2:3": (1056, 1584),
+    }
+    width, height = aspect_ratios[ratio]
+    outs = pipe(
+        prompt = prompt,
+        negative_prompt = " ",
+        width = width,
+        height=height,
+        true_cfg_scale=1.0,
+        num_inference_steps=8,
+    )
+    return outs.images[0]
